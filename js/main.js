8a7a712d7de9bb9988ac130f7fc424c6c98177ce
Noa Kim <noanonoa@gmail.com>
2020-09-24 00:41:55 -0700

    mobile first design

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 const hamburgerMenu = document.getElementById("hamburgerMenu")
 const barGroup = document.getElementById("barGroup")
+const bar = document.querySelector(".bar")
+const navReg = document.getElementById("nav-reg")
 
-const clickHandler = (e) => {
-  if (e.target.id == hamburgerMenu.id || barGroup) {
-    console.log(`on click, target id = ${e.target.id}`)
+document.addEventListener('click', (e) => {
+  if (e.target.id == hamburgerMenu.id) {
+    if (!navReg.classList.contains("nav-active")) {
+      navReg.classList.add("nav-active")
+    } else if (navReg.classList.contains("nav-active")) {
+      navReg.classList.remove("nav-active")
+    }
+  } else if (e.target.id == barGroup.id) {
+    if (!navReg.classList.contains("nav-active")) {
+      navReg.classList.add("nav-active")
+    } else if (navReg.classList.contains("nav-active")) {
+      navReg.classList.remove("nav-active")
+    }
+  } else if (e.target.className == "bar") {
+    if (!navReg.classList.contains("nav-active")) {
+      navReg.classList.add("nav-active")
+    } else if (navReg.classList.contains("nav-active")) {
+      navReg.classList.remove("nav-active")
+    } 
+  } else if (e.target.id != navReg) {
+    if (navReg.classList.contains("nav-active")) {
+      navReg.classList.remove("nav-active")
+    }
   }
-}
-
-document.addEventListener("click", clickHandler)
+})
